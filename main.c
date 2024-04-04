c28a6832fb072b27efaf92ed69894ec629965456
Luis Cedeno <59905362+Lcedeno14@users.noreply.github.com>
2024-04-04 08:07:38 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    printf("Banker's Algorithm\n"
+           "------------------\n"
+           "1) Enter claim graph\n"
+           "2) Request resource\n"
+           "3) Release resource\n"
+           "4) Determine safe sequence\n"
+           "5) Quit program\n");
+    return 0;
+}
