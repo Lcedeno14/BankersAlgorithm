61cf2c7f15c9fb08b0d8bf02beba8061bf0e7bc0
Luis Cedeno <59905362+Lcedeno14@users.noreply.github.com>
2024-04-08 21:56:19 -0700

    created input.txt for test inputs. finish creating graph and printing for selection 1

--- main.c
+++ main.c
@@ -1,12 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define Max_Resources 25
+#define Max_Processes 25
+
+int totalR, totalP; //total resources and processes the user chooses during claimGraph
+int resource[Max_Resources];
+int available[Max_Resources];
+int maxClaim[Max_Processes][Max_Resources];
+int allocated[Max_Processes][Max_Resources];
+int need[Max_Processes][Max_Resources];
+void printData(int choice){
+    //todo fix spacing
+    if(choice) {
+        printf("\nResources: \n");
+        for (int i = 0; i < totalR; i++) {
+            printf("%6s%d", "r", i);
+        }
+        printf("\n");
+
+        for (int i = 0; i < totalR; i++) {
+            if(!i ) {
+                printf("%8d", resource[i]);
+            }
+            else{printf("%6d", resource[i]);}
+        }
+        printf("\n   ");
+    }
+    printf("\n\nAvailable:\n   ");
+    for (int i = 0; i < totalR; i++) {
+        printf("%6s%d", "r", i);
+    }
+    printf("\n  ");
+    for (int i = 0; i < totalR; i++) {
+        printf("%7d", available[i]);
+    }
+
+    // Max Claim
+    printf("\n\nMax Claim:\n    ");
+    for (int i = 0; i < totalR; i++) {
+        printf("%6s%d", "r", i);
+    }
+    for (int i = 0; i < totalP; i++) {
+        printf("\np%d ", i);
+        for (int j = 0; j < totalR; j++) {
+            printf("%7d", maxClaim[i][j]);
+        }
+    }
+
+    // Allocated resources
+    printf("\n\nAllocated:\n    ");
+    for (int i = 0; i < totalR; i++) {
+        printf("%5s%d", "r", i);
+    }
+    for (int i = 0; i < totalP; i++) {
+        printf("\np%d ", i);
+        for (int j = 0; j < totalR; j++) {
+            printf("%6d", allocated[i][j]);
+        }
+    }
+
+    // Need matrix
+    printf("\n\nNeed:\n    ");
+    for (int i = 0; i < totalR; i++) {
+        printf("%5s%d", "r", i);
+    }
+    for (int i = 0; i < totalP; i++) {
+        printf("\np%d ", i);
+        for (int j = 0; j < totalR; j++) {
+            printf("%6d", need[i][j]);
+        }
+    }
+    printf("\n\n\n");
+}
+void enterClaimGraph(){
+    printf("Enter number of resources: \n");
+    scanf("%d", &totalR);
+    printf("Enter number of units for resources (r0 to r%d): \n", totalR - 1);
+    for(int i = 0; i<totalR; i++){
+        scanf("%d", &resource[i]);
+        available[i]=resource[i];
+    }
+    printf("Enter number of processes: \n");
+    scanf("%d", &totalP);
+    for(int i = 0; i<totalP; i++){
+        printf("Enter maximum number of units process p%d will "
+               "claim from each resource (r0 to r%d): \n", i,totalR-1);
+        for(int j = 0; j<totalR; j++){
+            scanf("%d", &maxClaim[i][j]);
+            need[i][j] = maxClaim[i][j];
+        }
+    }
+    for(int i = 0; i<totalP; i++){
+        printf("Enter number of units of each resource (r0 to r%d) "
+               "currently allocated to process p%d: \n", totalR-1,i);
+        for(int j = 0; j<totalR; j++){
+            //todo create check for allocating more than available
+            scanf("%d", &allocated[i][j]);
+            need[i][j] -= allocated[i][j];
+            available[j] -= allocated[i][j];
+        }
+    }
+    printData(1);
+}
+void requestResource(){ }
+void releaseResource(){}
+void determineSafeSequence(){}
+void quit(){
+    exit(0);
+};
+
 int main() {
-    printf("Banker's Algorithm\n"
-           "------------------\n"
-           "1) Enter claim graph\n"
-           "2) Request resource\n"
-           "3) Release resource\n"
-           "4) Determine safe sequence\n"
-           "5) Quit program\n");
-    return 0;
+    int input;
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+    while(1) {
+        printf("Banker's Algorithm\n"
+               "------------------\n"
+               "1) Enter claim graph\n"
+               "2) Request resource\n"
+               "3) Release resource\n"
+               "4) Determine safe sequence\n"
+               "5) Quit program\n"
+               "\nEnter Selection:");
+        scanf("%d", &input);
+
+        switch (input) {
+            case 1:
+                enterClaimGraph();
+                break;
+            case 2:
+                requestResource();
+                break;
+            case 3:
+                releaseResource();
+                break;
+            case 4:
+                determineSafeSequence();
+                break;
+            case 5:
+                quit();
+                break;
+            default:
+                printf("Invalid input, please try again. \n");
+        }
+    }
+#pragma clang diagnostic pop
+
 }
